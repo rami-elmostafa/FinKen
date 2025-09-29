657943f18c57ca13e9037cf5e2ab5fc26e5e65e3
Ethan Dillon <ethanbradforddillon@gmail.com>
2025-09-29 17:16:03 -0400

    Add backend forgot password route

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def create_new_user():
     # Change this to whatever the new user creation page is once it is created
     return render_template('CreateNewUser.html')
 
+@app.route("/ForgotPassword")
+def forgot_password():
+    return render_template('ForgotPassword.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
