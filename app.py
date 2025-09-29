5ef76cae40e655621a0f56c79ba863872ea7f3cc
Ethan Dillon <ethanbradforddillon@gmail.com>
2025-09-29 17:17:45 -0400

    Update forgot password link to the button

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def create_new_user():
 
 @app.route("/ForgotPassword")
 def forgot_password():
+    # Change this to whatever the new forgot password page is once it is created
     return render_template('ForgotPassword.html')
 
 if __name__ == '__main__':
