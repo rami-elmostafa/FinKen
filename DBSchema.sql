738a42ed823a69748d4383a752eaadcc927be465
Ethan Dillon <ethanbradforddillon@gmail.com>
2025-10-10 19:29:39 -0400

    Update DB Schema

--- DBSchema.sql
+++ DBSchema.sql
@@ -1,6 +1,45 @@
 -- WARNING: This schema is for context only and is not meant to be run.
 -- Table order and constraints may not be valid for execution.
 
+CREATE TABLE public.chart_of_accounts (
+  accountid integer GENERATED ALWAYS AS IDENTITY NOT NULL,
+  accountnumber text NOT NULL UNIQUE,
+  accountname text NOT NULL UNIQUE,
+  accountdescription text,
+  normalside text NOT NULL CHECK (normalside = ANY (ARRAY['Debit'::text, 'Credit'::text])),
+  category text NOT NULL,
+  subcategory text,
+  initialbalance numeric NOT NULL DEFAULT 0.00,
+  displayorder integer,
+  statementtype text CHECK (statementtype = ANY (ARRAY['BS'::text, 'IS'::text, 'RE'::text])),
+  isactive boolean NOT NULL DEFAULT true,
+  datecreated timestamp with time zone NOT NULL DEFAULT now(),
+  createdbyuserid integer NOT NULL,
+  comment text,
+  CONSTRAINT chart_of_accounts_pkey PRIMARY KEY (accountid),
+  CONSTRAINT fk_chartofaccounts_user FOREIGN KEY (createdbyuserid) REFERENCES public.users(UserID)
+);
+CREATE TABLE public.cron_job_logs (
+  id integer NOT NULL DEFAULT nextval('cron_job_logs_id_seq'::regclass),
+  job_name character varying NOT NULL,
+  execution_time timestamp with time zone DEFAULT now(),
+  status character varying NOT NULL,
+  response_body text,
+  notes text,
+  CONSTRAINT cron_job_logs_pkey PRIMARY KEY (id)
+);
+CREATE TABLE public.event_logs (
+  logid integer GENERATED ALWAYS AS IDENTITY NOT NULL,
+  userid integer NOT NULL,
+  timestamp timestamp with time zone NOT NULL DEFAULT now(),
+  actiontype text NOT NULL,
+  tablename text NOT NULL,
+  recordid integer NOT NULL,
+  beforevalue jsonb,
+  aftervalue jsonb,
+  CONSTRAINT event_logs_pkey PRIMARY KEY (logid),
+  CONSTRAINT fk_eventlogs_user FOREIGN KEY (userid) REFERENCES public.users(UserID)
+);
 CREATE TABLE public.password_history (
   PasswordHistoryID integer GENERATED ALWAYS AS IDENTITY NOT NULL,
   UserID integer NOT NULL,
@@ -70,6 +109,8 @@ CREATE TABLE public.users (
   SuspensionEndDate timestamp with time zone,
   DateCreated timestamp with time zone NOT NULL DEFAULT now(),
   SuspensionReason text,
+  AuthUID uuid UNIQUE,
   CONSTRAINT users_pkey PRIMARY KEY (UserID),
-  CONSTRAINT fk_user_role FOREIGN KEY (RoleID) REFERENCES public.roles(RoleID)
+  CONSTRAINT fk_user_role FOREIGN KEY (RoleID) REFERENCES public.roles(RoleID),
+  CONSTRAINT fk_users_auth FOREIGN KEY (AuthUID) REFERENCES auth.users(id)
 );
